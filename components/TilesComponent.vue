bbcfd4048146efe1273f4bcb25169c11a23a6119
theoluwafemi <oluwafemidamilola21@gmail.com>
2023-01-11 14:21:34 +0100

    Fetch data from endpoint and add cards to grid

---
+++ components/TilesComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tile">
+    <img class="tile-img" :src="item.urls.regular" :alt="item.alt_description">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: false,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tile {
+  margin: 0;
+  // background-repeat: no-repeat;
+  // background-size: cover;
+  // background-position: top;
+
+  &-img {
+    width: 100%;
+    height: 100%;
+    display: block;
+    object-fit: cover;
+  }
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,217 @@
 <template>
-  <section></section>
+  <section class="home">
+    <template v-if="content.length">
+      <div class="tiles-container">
+        <div class="title">Connecting people and spaces</div>
+        <TilesComponent
+          v-for="(item, index) in content"
+          :key="index"
+          :item="item"
+          :class="getClass(index)"
+        />
+        <div class="button">
+          <button class="btn" @click.prevent="$fetch">Load More</button>
+        </div>
+      </div>
+    </template>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      content: [],
+      limit: 7,
+      page: 1,
+    }
+  },
+  fetchOnServer: false,
+
+  async fetch() {
+    const { env } = this.$nuxt.context
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/photos?client_id=${env.NUXT_ENV_UNSPLASH_ACCESS_KEY}&page=${this.page}&per_page=${this.limit}`
+      )
+
+      if (!response.ok)
+        return console.error('An error occurred, please refresh to try again')
+
+      this.content = [...this.content, ...(await response.json())]
+      this.page++
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  methods: {
+    getClass(index) {
+      const classes = [
+        'featured',
+        'card-1',
+        'card-2',
+        'card-3',
+        'card-4',
+        'card-5',
+        'card-6',
+      ]
+      return classes[index % classes.length]
+    },
+    //     round() {
+    //       tags_occurrences = {"xs": 2, "sm": 1, "md": 1, "lg": 1, "xl": 1}
+
+    // cards = [i for i in range(10)]
+
+    // tag_index = 0
+    // for i, card in enumerate(cards):
+    //     tag = list(tags_occurrences.keys())[tag_index % len(tags_occurrences)]
+    //     print(f"Card {card} is tagged with {tag}")
+    //     tags_occurrences[tag]-=1
+    //     if tags_occurrences[tag] == 0:
+    //         tag_index += 1
+
+    //     }
+  },
 }
 </script>
+
+<style lang="scss">
+.home {
+  background-color: #191919;
+  min-height: 100vh;
+  padding: 4.375rem;
+  color: #fff;
+
+  @media only screen and (max-width: 900px) {
+    padding: 2rem;
+  }
+
+  @media only screen and (max-width: 600px) {
+    padding: 1rem;
+  }
+
+  .tiles-container {
+    display: grid;
+    gap: 1.25rem;
+    max-width: 100%;
+    grid-template-columns: 1fr;
+    grid-auto-rows: 168px;
+    // grid-auto-flow: dense;
+
+    @media only screen and (min-width: 1024px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+
+    @media only screen and (min-width: 768px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media only screen and (min-width: 600px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media only screen and (min-width: 1440px) {
+      grid-template-columns: repeat(7, 1fr);
+      grid-auto-rows: 90px;
+    }
+
+    .featured {
+      @media only screen and (min-width: 600px) {
+        grid-column: auto / span 1;
+        grid-row: auto / span 3;
+      }
+
+      @media only screen and (min-width: 768px) {
+        grid-column: auto / span 1;
+      }
+
+      @media only screen and (min-width: 1024px) {
+        grid-column: auto / span 2;
+      }
+
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 4;
+        grid-row: auto / span 4;
+      }
+    }
+
+    .card-1 {
+      @media only screen and (min-width: 768px) {
+        grid-column: auto / span 1;
+        grid-row: auto / span 2;
+      }
+
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 2;
+        grid-row: auto / span 3;
+      }
+    }
+
+    .card-2 {
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 2;
+        grid-row: auto / span 2;
+      }
+    }
+
+    .card-3 {
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 1;
+        grid-row: auto / span 2;
+      }
+    }
+
+    .card-4 {
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 2;
+        grid-row: auto / span 4;
+      }
+    }
+
+    .card-5 {
+      @media only screen and (min-width: 1440px) {
+        grid-column: auto / span 2;
+        grid-row: auto / span 3;
+      }
+    }
+
+    .card-6 {
+      @media only screen and (min-width: 1440px) {
+        grid-column: 1 / span 2;
+        grid-row: auto / span 2;
+      }
+    }
+
+    .title {
+      font-weight: 400;
+      font-size: 28px;
+      line-height: 44px;
+      letter-spacing: -0.04em;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      @media only screen and (min-width: 1440px) {
+        grid-column: 1 / span 2;
+      }
+
+      @media only screen and (max-width: 1024px) {
+        grid-column: 1 / span 2;
+      }
+
+      @media only screen and (max-width: 768px) {
+        grid-column: 1 / span 2;
+      }
+    }
+
+    .button {
+      @media only screen and (min-width: 1440px) {
+        grid-column: 6 / span 2;
+        grid-row: auto / span 2;
+      }
+    }
+  }
+}
+</style>
