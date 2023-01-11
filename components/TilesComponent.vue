7a2dd7fb772f2fe8d9b03f77c7db014b1e338202
theoluwafemi <oluwafemidamilola21@gmail.com>
2023-01-11 23:03:08 +0100

    implement grid and hover effect

--- components/TilesComponent.vue
+++ components/TilesComponent.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="tile">
-    <img class="tile-img" :src="item.urls.regular" :alt="item.alt_description">
+    <div class="overlay">
+      <div class="overlay-info">
+        <p class="name">{{ item.id }}</p>
+        <p class="desc">{{ item.alt_description }}</p>
+      </div>
+    </div>
+
+    <div class="tile-icon">
+      <img src="../assets/icons/chevron-right.svg" alt="chevron right" />
+    </div>
+    <img
+      class="tile-img"
+      :src="item.urls.regular"
+      :alt="item.alt_description" width="20" height="20"
+    />
   </div>
 </template>
 
@@ -21,9 +35,16 @@ export default {
 <style lang="scss" scoped>
 .tile {
   margin: 0;
-  // background-repeat: no-repeat;
-  // background-size: cover;
-  // background-position: top;
+  position: relative;
+  transition: all 0.4s ease;
+
+  &:hover {
+    .overlay {
+      visibility: visible;
+      opacity: 1;
+      height: 100%;
+    }
+  }
 
   &-img {
     width: 100%;
@@ -31,6 +52,38 @@ export default {
     display: block;
     object-fit: cover;
   }
+}
 
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 0%;
+  top: 0;
+  left: 0;
+  background: #4a52ff;
+  cursor: pointer;
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.4s ease-in-out;
+  padding: 1rem;
+
+    .name {
+      font-size: 0.875rem;
+      text-transform: uppercase;
+      line-height: 2;
+      font-weight: bold;
+    }
+
+    .desc {
+      font-size: .857rem;
+      text-transform: capitalize;
+    }
+
+}
+
+.tile-icon {
+  position: absolute;
+  bottom: 1.25rem;
+  right: 1.25rem;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="home">
     <template v-if="content.length">
-      <div class="tiles-container">
+      <div ref="tilesContainer" class="tiles-container">
         <div class="title">Connecting people and spaces</div>
         <TilesComponent
           v-for="(item, index) in content"
           :key="index"
+          :ref="`card${index}`"
           :item="item"
           :class="getClass(index)"
         />
@@ -28,8 +29,6 @@ export default {
     }
   },
 
-  fetchOnServer: false,
-
   async fetch() {
     const { env } = this.$nuxt.context
     const url = new URL('https://api.unsplash.com/photos')
@@ -37,10 +36,9 @@ export default {
       client_id: env.NUXT_ENV_UNSPLASH_ACCESS_KEY,
       page: this.page,
       per_page: this.limit,
-    }).toString();
+    }).toString()
     try {
-      const response = await fetch(`${url}?${urlParams}`
-      )
+      const response = await fetch(`${url}?${urlParams}`)
       if (!response.ok)
         return console.error('An error occurred while trying to fetch images')
       this.content = [...this.content, ...(await response.json())]
@@ -49,6 +47,19 @@ export default {
       console.error(error)
     }
   },
+
+  watch: {
+    content: function (val) {
+      if (val.length > 7) {
+        const { tilesContainer } = this.$refs
+        if (!tilesContainer.style.gridAutoFlow) {
+          tilesContainer.style.gridAutoFlow = 'dense'
+        }
+      }
+    },
+  },
+
+  fetchOnServer: false,
 
   methods: {
     getClass(index) {
@@ -73,13 +84,14 @@ export default {
   min-height: 100vh;
   padding: 4.375rem;
   color: #fff;
+  padding: 1rem;
 
-  @media only screen and (max-width: 900px) {
+  @media only screen and (min-width: 768px) {
     padding: 2rem;
   }
 
-  @media only screen and (max-width: 600px) {
-    padding: 1rem;
+  @media only screen and (min-width: 1024px) {
+    padding: 4rem;
   }
 
   .tiles-container {
@@ -87,24 +99,24 @@ export default {
     gap: 1.25rem;
     max-width: 100%;
     grid-template-columns: 1fr;
-    grid-auto-rows: 168px;
+    grid-auto-rows: 10.5rem;
     // grid-auto-flow: dense;
 
-    @media only screen and (min-width: 1024px) {
-      grid-template-columns: repeat(4, 1fr);
+    @media only screen and (min-width: 600px) {
+      grid-template-columns: repeat(2, 1fr);
     }
 
     @media only screen and (min-width: 768px) {
       grid-template-columns: repeat(3, 1fr);
     }
 
-    @media only screen and (min-width: 600px) {
-      grid-template-columns: repeat(2, 1fr);
+    @media only screen and (min-width: 1024px) {
+      grid-template-columns: repeat(4, 1fr);
     }
 
     @media only screen and (min-width: 1440px) {
-      grid-template-columns: repeat(7, 1fr);
-      grid-auto-rows: 90px;
+      grid-template-columns: repeat(12, 1fr);
+      grid-auto-rows: 4.875rem;
     }
 
     .featured {
@@ -122,7 +134,7 @@ export default {
       }
 
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 4;
+        grid-column: auto / span 6;
         grid-row: auto / span 4;
       }
     }
@@ -134,71 +146,83 @@ export default {
       }
 
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 2;
-        grid-row: auto / span 3;
+        grid-column: auto / span 4;
+        grid-row: auto / span 2;
       }
     }
 
     .card-2 {
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 2;
+        grid-column: auto / span 3;
         grid-row: auto / span 2;
       }
     }
 
     .card-3 {
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 1;
+        grid-column: auto / span 2;
         grid-row: auto / span 2;
       }
     }
 
     .card-4 {
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 2;
+        grid-column: auto / span 4;
         grid-row: auto / span 4;
       }
     }
 
     .card-5 {
       @media only screen and (min-width: 1440px) {
-        grid-column: auto / span 2;
+        grid-column: auto / span 3;
         grid-row: auto / span 3;
       }
     }
 
     .card-6 {
       @media only screen and (min-width: 1440px) {
-        grid-column: 1 / span 2;
+        grid-column: auto / span 3;
         grid-row: auto / span 2;
       }
     }
 
     .title {
       font-weight: 400;
-      font-size: 28px;
-      line-height: 44px;
+      font-size: 1.5rem;
+      line-height: 2.75rem;
       letter-spacing: -0.04em;
       display: flex;
       justify-content: flex-start;
       align-items: center;
 
+      @media only screen and (min-width: 768px) {
+        grid-column: 1 / span 2;
+        font-size: 2rem;
+      }
+
+      @media only screen and (min-width: 1024px) {
+        grid-column: 1 / span 2;
+      }
+
       @media only screen and (min-width: 1440px) {
-        grid-column: 1 / span 2;
-      }
-
-      @media only screen and (max-width: 1024px) {
-        grid-column: 1 / span 2;
-      }
-
-      @media only screen and (max-width: 768px) {
-        grid-column: 1 / span 2;
+        grid-column: 1 / span 4;
+        grid-row: 1 / span 2;
       }
     }
 
     .button {
+      .btn {
+        margin-top: .5rem;
+        color: #fff;
+        text-transform: uppercase;
+        padding: .5rem 1.25rem;
+        background: #4a52ff;
+        border-radius: 1.563rem;
+        cursor: pointer;
+        outline: none;
+      }
       @media only screen and (min-width: 1440px) {
-        grid-column: 6 / span 2;
+        grid-column: 10 / span 2;
         grid-row: auto / span 2;
       }
     }
